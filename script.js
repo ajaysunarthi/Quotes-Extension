0488973c62d2e423e53cd40e83ba80fdc8bb9bfb
ajay <ajaysunarthi@iitj.ac.in>
2017-01-06 10:19:17 +0530

    fatching quotes

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-var $  = document.getElementById.bind(document);
+var $ = document.getElementById.bind(document);
 
-var quoteApp = function (el) {
-	this.el = el;
+var quoteApp = function(el) {
+    this.el = el;
 
-	if(this.quotes){
-		// fetch some quotes
-	}else {
-		// you have quotes now render DOM
-	}
+    if (this.quotes) {
+        // you have quotes now render DOM
+    } else {
+        this.loadQuotes();
+        // fetch some quotes
+    }
+}
+
+quoteApp.prototype.loadQuotes = function() {
+    this.Ajax('./quotes.json');
+}
+
+quoteApp.prototype.Ajax = function(url) {
+
+    var xhr = new XMLHttpRequest();
+    var self = this;
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = xhr.responseText;
+            console.log((JSON.parse(response) ));
+        }
+    };
+
+    xhr.open('GET', chrome.extension.getURL(url), true);
+    xhr.send();
+
 }
 
 window.quoteApp = new quoteApp($('app'));
